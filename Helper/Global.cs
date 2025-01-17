5248ec1b0e9b7ece63c7ae508995b27d0379e395
Tyler Parret True <mysteryworldgod@outlook.com>
2025-01-17 17:31:50 +0800

    add Global

---
+++ Helper/Global.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helper {
+	public static class Global {
+		public static Worker Worker { get; } = new();
+		public static PatternManager PatternManager { get; } = new();
+		internal static WndHandler WndHandler { get; } = new();
+	}
+}
