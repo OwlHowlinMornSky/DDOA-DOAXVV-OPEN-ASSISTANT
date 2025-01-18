1dc97b8f0e160278e6bbe8a571eb996afd40eb71
Tyler Parret True <mysteryworldgod@outlook.com>
2025-01-18 10:37:12 +0800

    修复设置的初值问题

--- Helper/WorkSettings.cs
+++ Helper/WorkSettings.cs
@@ -6,7 +6,7 @@ namespace Helper {
 			public bool KeepAwake    = true;  // 阻止睡眠。
 			public bool KeepScreenOn = false; // 同时保持显示。
 		};
-		public static Global global;
+		public static Global global = new();
 
 		public struct WndHandler() {
 			public bool UseSendInput       = false; // 选择控制鼠标。
@@ -15,10 +15,10 @@ namespace Helper {
 			public bool Debug_ShowCapture  = true; // [调试] 是否显示截取到的帧。
 			public bool Debug_DebugHandler = false; // [调试] 是否以DDOA调试器为目标。
 		};
-		public static WndHandler wndHandler;
+		public static WndHandler wndHandler = new();
 
 		public struct Challenge() {
-			public int  PlayMatch             = 2;     // 0: Previous; 1: New; 2: Activity
+			public int  PlayMatch             = 0;     // 0: Previous; 1: New; 2: Activity
 			public int  SelectedActivityLevel = 6;     // -EDCBAS...
 			public int  SelectedActivityMatch = 1;     // 0123456...
 			public bool EnterAddition         = false; // 进入奖励挑战赛。默认不进入奖励挑战赛。
@@ -27,12 +27,12 @@ namespace Helper {
 			public bool AskForManual          = false; // 是否在开始时请求手动导航。默认不暂停。
 			public bool AutoUseDrink          = false; // 是否自动使用饮料补充FP。默认不使用饮料。
 		}
-		public static Challenge challenge;
+		public static Challenge challenge = new();
 
 		public struct Daily() {
 			public bool DoCheck = true; // 是否每日签到。默认进行每日签到。
 			public bool DoShot  = true; // 是否每日拍摄。默认进行每日拍摄。
 		}
-		public static Daily daily;
+		public static Daily daily = new();
 	}
 }
