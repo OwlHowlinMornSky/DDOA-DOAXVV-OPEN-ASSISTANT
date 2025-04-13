c07acd364e6aca2352e231c429deca000dfcc89c
Tyler Parret True <mysteryworldgod@outlook.com>
2025-04-13 15:46:50 +0800

    MakeTool增加选点，输出格式化。

--- MakeTool/TaskClipPicture.cpp
+++ MakeTool/TaskClipPicture.cpp
@@ -18,8 +18,12 @@ cv::Mat clippedShow;
 cv::Rect finalrect;
 std::filesystem::path filepath;
 
+std::vector<cv::Point> g_points;
+
 void OnMouseEvent_Processed(int msg, int x, int y, int flag, void* userdata) {
-	if (msg == cv::EVENT_RBUTTONUP) {
+	switch (msg) {
+	case cv::EVENT_RBUTTONUP:
+	{
 		auto t = time(0);
 
 		std::string tt = "[clip-" + std::to_string(t) + "]";
@@ -39,12 +43,26 @@ void OnMouseEvent_Processed(int msg, int x, int y, int flag, void* userdata) {
 		}
 
 		ofs << 1 << std::endl;
-		ofs << filename << ' ' << 1 << ' ' << 65535 << ' ';
-		ofs << finalrect.x << ' ' << finalrect.y << ' ' << finalrect.width << ' ' << finalrect.height << std::endl;
+		ofs << std::format("{0:<20s} 0  {1:5d}  {2:3d} {3:3d} {4:3d} {5:3d} {6:2d}", filename, 6554, finalrect.x, finalrect.y, finalrect.width, finalrect.height, g_points.size());
+
+		for (auto& p : g_points) {
+			ofs << std::format(" {0:3d} {1:3d}", p.x, p.y);
+		}
+		ofs << std::endl;
+
+		//ofs << filename << ' ' << 1 << ' ' << 65535 << ' ';
+		//ofs << finalrect.x << ' ' << finalrect.y << ' ' << finalrect.width << ' ' << finalrect.height << std::endl;
 
 		ofs.close();
 
 		cv::destroyWindow(ClippedWndName);
+		break;
+	}
+	case cv::EVENT_LBUTTONUP:
+	{
+		g_points.emplace_back(x, y);
+		break;
+	}
 	}
 }
 
@@ -90,7 +108,8 @@ void OnMouseEvent_show(int msg, int x, int y, int flag, void* userdata) {
 
 				clipped = original(r);
 
-				cv::namedWindow(ClippedWndName, cv::WindowFlags::WINDOW_FREERATIO);
+				g_points.clear();
+				cv::namedWindow(ClippedWndName);
 				cv::setMouseCallback(ClippedWndName, OnMouseEvent_Processed);
 				cv::imshow(ClippedWndName, clipped);
 			}
